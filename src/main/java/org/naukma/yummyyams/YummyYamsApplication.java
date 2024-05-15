d9ac58e1efa37d79b745cb1cf7a45a93fa7dffdb
Oleksandr Semytskyi <oleksandr.semytskyi@gmail.com>
2024-05-15 22:10:28 +0300

    init commit

---
+++ src/main/java/org/naukma/yummyyams/YummyYamsApplication.java
@@ -0,0 +1,13 @@
+package org.naukma.yummyyams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YummyYamsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YummyYamsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/naukma/yummyyams/YummyYamsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.naukma.yummyyams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YummyYamsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

c745947d58bf0de21d4fecac386ac2a40479f092
Oleksandr Semytskyi <oleksandr.semytskyi@gmail.com>
2024-05-22 20:50:07 +0300

    add cross origin

--- src/main/java/org/naukma/yummyyams/category/CategoryController.java
+++ src/main/java/org/naukma/yummyyams/category/CategoryController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.naukma.yummyyams.category.dto.CategoryCreateUpdateDto;
 import org.naukma.yummyyams.category.dto.CategoryResponseDto;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/category")
+@CrossOrigin(origins = "http://localhost:3000")
 @RequiredArgsConstructor
 public class CategoryController {
     private final CategoryService service;
--- src/main/java/org/naukma/yummyyams/user/UserController.java
+++ src/main/java/org/naukma/yummyyams/user/UserController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.naukma.yummyyams.user.dto.UserCreateUpdateDto;
 import org.naukma.yummyyams.user.dto.UserResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/user")
+@CrossOrigin(origins = "http://localhost:3000")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService service;

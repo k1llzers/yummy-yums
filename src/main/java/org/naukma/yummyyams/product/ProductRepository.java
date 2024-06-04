8836458d16cf5d4faabb1975e6e41b9f650dbba8
Oleksandr Semytskyi <oleksandr.semytskyi@gmail.com>
2024-06-04 21:05:00 +0300

    refactor query

--- src/main/java/org/naukma/yummyyams/product/ProductRepository.java
+++ src/main/java/org/naukma/yummyyams/product/ProductRepository.java
@@ -12,12 +12,7 @@ public interface ProductRepository extends JpaRepository<ProductEntity, Integer>
     @Query(nativeQuery = true, value = "SELECT * FROM product ORDER BY similarity(:input, product.name) DESC LIMIT :limit")
     List<ProductEntity> findAllOrderBySimilarity(@Param("input") String input, @Param("limit") Integer limit);
 
-    @Query("select (count(p) > 0) from ProductEntity p where p.name like '%:input%'")
-    Boolean canBeAddedToRecipe(@Param("input") String input);
-
     @Query("select (count(p) > 0) from ProductEntity p where p.name like CONCAT('% ', :name, ' %') or " +
             "p.name like CONCAT(:name, ' %') or p.name like CONCAT('% ', :name) or p.name = :name")
-    boolean existsByNameLike(String name);
-
-
+    Boolean canBeAddedToRecipe(@Param("input") String input);
 }

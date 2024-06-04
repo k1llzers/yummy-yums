1b75bc9460e3f7f4869f5203081ea26c097c1528
Oleksandr Semytskyi <o.semytskyi@itvsystems.com.ua>
2024-06-04 23:07:15 +0300

    work with criteria api

--- src/main/java/org/naukma/yummyyams/recipe/RecipeService.java
+++ src/main/java/org/naukma/yummyyams/recipe/RecipeService.java
@@ -1,31 +1,45 @@
 package org.naukma.yummyyams.recipe;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.*;
 import lombok.RequiredArgsConstructor;
 import org.naukma.yummyyams.base.EntityNotFoundMessage;
 import org.naukma.yummyyams.base.service.BaseService;
 import org.naukma.yummyyams.category.CategoryEntity;
 import org.naukma.yummyyams.recipe.dto.RecipeCreateUpdateDto;
 import org.naukma.yummyyams.recipe.dto.RecipeShortResponseDto;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
-
-import static org.springframework.data.jpa.domain.Specification.where;
+import java.util.Set;
 
 @EntityNotFoundMessage(errorMessage = "Can`t find recipe by id")
 @Service
 @RequiredArgsConstructor
 @Transactional(propagation = Propagation.REQUIRED)
 public class RecipeService extends BaseService<RecipeEntity, RecipeCreateUpdateDto, Integer> {
+    private final EntityManager em;
+
     public List<RecipeShortResponseDto> getAll() {
-        return ((RecipeMapper) mapper).toShortResponseList(((RecipeRepository)repository)
-                .findAll(where(hasCategory(null))));
+        return ((RecipeMapper) mapper).toShortResponseList(findRecipeByCategoryNameAndProducts(null, null, null));
     }
 
-    private static Specification<RecipeEntity> hasCategory(CategoryEntity entity) {
-        return (root, query, cb) -> cb.equal(root.get("category"), entity);
+    public List<RecipeEntity> findRecipeByCategoryNameAndProducts(Integer categoryId, String name, Set<String> products) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<RecipeEntity> cq = cb.createQuery(RecipeEntity.class);
+
+        Root<RecipeEntity> recipe = cq.from(RecipeEntity.class);
+        List<Predicate> predicates = new ArrayList<>();
+
+        if (categoryId != null) {
+            Join<Object, Object> category = recipe.join("category", JoinType.LEFT);
+            predicates.add(cb.equal(category.get("id"), categoryId));
+        }
+        cq.where(predicates.toArray(new Predicate[0]));
+
+        return em.createQuery(cq).getResultList();
     }
 }

00de631f39695d00b17231060a329ed7557b4a62
Oleksandr Semytskyi <o.semytskyi@itvsystems.com.ua>
2024-06-04 22:51:45 +0300

    try to add specification

--- src/main/java/org/naukma/yummyyams/recipe/RecipeRepository.java
+++ src/main/java/org/naukma/yummyyams/recipe/RecipeRepository.java
@@ -1,6 +1,7 @@
 package org.naukma.yummyyams.recipe;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface RecipeRepository extends JpaRepository<RecipeEntity, Integer> {
+public interface RecipeRepository extends JpaRepository<RecipeEntity, Integer>, JpaSpecificationExecutor<RecipeEntity> {
     List<RecipeEntity> findAllByCategory_Id(Integer category_id);
 
     List<RecipeEntity> findAllByNameLikeIgnoreCase(String name);
--- src/main/java/org/naukma/yummyyams/recipe/RecipeService.java
+++ src/main/java/org/naukma/yummyyams/recipe/RecipeService.java
@@ -3,13 +3,17 @@ package org.naukma.yummyyams.recipe;
 import lombok.RequiredArgsConstructor;
 import org.naukma.yummyyams.base.EntityNotFoundMessage;
 import org.naukma.yummyyams.base.service.BaseService;
+import org.naukma.yummyyams.category.CategoryEntity;
 import org.naukma.yummyyams.recipe.dto.RecipeCreateUpdateDto;
 import org.naukma.yummyyams.recipe.dto.RecipeShortResponseDto;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
+import static org.springframework.data.jpa.domain.Specification.where;
 
 @EntityNotFoundMessage(errorMessage = "Can`t find recipe by id")
 @Service
@@ -17,6 +21,11 @@ import java.util.List;
 @Transactional(propagation = Propagation.REQUIRED)
 public class RecipeService extends BaseService<RecipeEntity, RecipeCreateUpdateDto, Integer> {
     public List<RecipeShortResponseDto> getAll() {
-        return ((RecipeMapper) mapper).toShortResponseList(getAllEntities());
+        return ((RecipeMapper) mapper).toShortResponseList(((RecipeRepository)repository)
+                .findAll(where(hasCategory(null))));
+    }
+
+    private static Specification<RecipeEntity> hasCategory(CategoryEntity entity) {
+        return (root, query, cb) -> cb.equal(root.get("category"), entity);
     }
 }

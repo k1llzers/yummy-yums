59ba059a226b03eb6bc742791c5069db158386f1
Oleksandr Semytskyi <oleksandr.semytskyi@gmail.com>
2024-06-09 22:32:51 +0300

    make ProductDto serialize correct

--- src/main/java/org/naukma/yummyyams/product/dto/ProductDto.java
+++ src/main/java/org/naukma/yummyyams/product/dto/ProductDto.java
@@ -1,20 +1,32 @@
 package org.naukma.yummyyams.product.dto;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.SneakyThrows;
 import org.naukma.yummyyams.product.Store;
 
-@Data
+@Setter
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class ProductDto {
+    private static ObjectMapper mapper = new ObjectMapper();
+
     private Integer id;
     private String name;
     private Double price;
     private String weight;
     private String imgUrl;
     private Store store;
+
+    @Override
+    @SneakyThrows
+    public String toString() {
+        return mapper.writeValueAsString(this);
+    }
 }
